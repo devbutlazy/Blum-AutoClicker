35538f3c5920cb31a6ede6b9dd4c4054918bc74a
LazyDev <devbutlazy@gmail.com>
2024-12-30 11:23:23 +0200

    Probanly last commit to this project, thanks a lot for this special journey :love: [In new override: deleted the share your win button, so it won't trigger it anymore

--- core/clicker/blum.py
+++ core/clicker/blum.py
@@ -20,7 +20,6 @@ from typing import Tuple, Any
 class BlumClicker:
     def __init__(self):
         self.utils = Utilities()
-        self.whitelist_windows: list[str] = get_config_value("WHITELIST_WINDOWS")
 
         self.paused: bool = True
         self.replay_limit_logged = False
@@ -269,16 +268,6 @@ class BlumClicker:
 
         return False
 
-    def close_extra_windows(self):
-        """
-        Close Telegram app.
-
-        :return: None
-        """
-        if (window := pwc.getActiveWindow()).title not in self.whitelist_windows:
-            window.hide()
-            logger.error(f"Unwanted window hidden ({window.title})")
-
     @staticmethod
     def reload_overrides() -> None:
         """
@@ -382,13 +371,9 @@ class BlumClicker:
         Runs the clicker.
         """
         try:
-            self.whitelist_windows.append(self.utils.get_terminal_name())
-    
             window = self.utils.get_window()
             if not window:
                 return logger.error(get_language("WINDOW_NOT_FOUND"))
-            
-            self.whitelist_windows.append(window.title)
             
             logger.info(get_language("CLICKER_INITIALIZED"))
             logger.info(get_language("FOUND_WINDOW").format(window=window.title))
@@ -397,8 +382,6 @@ class BlumClicker:
             while True:
                 if await self.handle_input():
                     continue
-
-                self.close_extra_windows()
 
                 rect = self.utils.get_rect(window)
 
